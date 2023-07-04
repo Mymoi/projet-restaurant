103937ebdb2921736d3ba08806edf9cc6c4796b2
Myriam Rajeriarisoa <myrajeriarisoa@yahoo.fr>
2023-07-04 22:48:12 +0200

    rajout javascript pour le formulaire nouveau client

---
+++ script.js
@@ -0,0 +1,53 @@
+// ECF - Evaluation Graduate Développeur Web et Web Mobile
+// Réalisation par : Voahiranirina Myriam RAJERIARISOA
+// Date : Mai 2023
+// Projet : création du site internet du restaurant Quai Antique 
+// Phases du projet : 3 découpage en 3 lots. Chaque lot livré  pour tout utilisateur.
+// ==> Lot 1 : site internet Front End statique
+// ==> Lot 2 : site internet Front End dynamique
+// ==> Lot 3: site internet avec les parties Front End  et Back end opérationnelles
+// Livraisons : V1.0 : 24 mai 2023 - Lot 1 - Partie Front-End statique avec Html et CSS
+
+
+//Prise en compte des informations du formulaire nouveau client
+const el = document.getElementById('register-custom-form')
+el.addEventListener('submit', (event) =>{
+  event.preventDefault()
+
+  let civility = event.target.female.checked ? 'female' : 'male'
+  let name = event.target.name.value
+  let category = event.target.category.value
+  let cgv = event.target.cgv.checked
+  
+
+  let myCivility = civility === 'female' ? 'Madame' : 'Monsieur'
+  let myName = name.toUpperCase()
+  let myCategory
+
+  switch (category) {
+    case "hunter-gather":
+      myCategory = "Cueilleur";
+      break;
+
+    case "gargantuan":
+      myCategory = "Gargantuesque";
+      break;
+
+    case "modern":
+        myCategory = "Moderne";
+        break;
+
+    default:
+      myCategory = "Aucune catégorie";
+      break;
+  } 
+
+
+  if (cgv) {
+    alert(`Merci pour votre enregistrement ${myCivility} ${myName}, \n vous recevrez prochainement notre newsletter et les nouveautés de votre catégorie préférée, ${myCategory} `)
+    }
+    else {
+    alert(`Merci de cocher la case des conditions générales ${myCivility} ${myName}, pour valider votre enregistrement`)
+    }
+ })
+
